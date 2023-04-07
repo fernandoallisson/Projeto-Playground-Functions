8ba233fd78b1e4e4711e99b11fc2a2b3fa0f37df
fernandoallisson <fernando.alisson@ufpe.br>
2023-04-07 18:29:58 -0300

    Resolução de exercício 6

--- src/challenges.js
+++ src/challenges.js
@@ -28,7 +28,7 @@ const MayorNumber = (array) => {
     }
   }
   return MayorNumberArray
-}
+};
 
 const highestCount = (array) => {
   let searchNumber = MayorNumber(array);
@@ -42,6 +42,22 @@ const highestCount = (array) => {
 };
 
 // Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
+
+const calcTriangleArea = (base, height) => {
+  return (base * height) / 2;
+};
+
+const calcRectangleArea = (base, height) => {
+  return base * height;
+};
+
+const calcAllAreas = (base, height, form) => {
+  if (form === 'triângulo') {
+    return `O valor da área do triângulo é de: ${calcTriangleArea(base, height)}`;
+  } else if (form === 'retângulo') {
+    return `O valor da área do retângulo é de: ${calcRectangleArea(base, height)}`;
+  } return `Não foi possível fazer o cálculo, insira uma forma geométrica válida`;
+};
 
 // Desafio 7 - Crie a função catAndMouse
 
