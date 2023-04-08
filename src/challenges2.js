2bb88a5f501e3647fa9e316f34fbee337c8041a5
fernandoallisson <fernando.alisson@ufpe.br>
2023-04-08 16:36:02 -0300

    Exercício bonus 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,4 +1,39 @@
 // Desafio 11 - Crie a função generatePhoneNumber
+const verifArray = (array) => {
+  let verifyNumber = true; 
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.'
+  }
+  for (let index in array) {
+    if (array[index] < 0 || array[index] > 9){
+      return 'não é possível gerar um número de telefone com esses valores'
+    }
+  }
+  let counter = {}; 
+  array.forEach(num => {
+    if (counter[num]) {
+      counter[num] += 1; 
+    } else {
+      counter[num] = 1
+    }
+  });
+    for (let numb in counter) {
+      if (counter[numb] > 3) {
+        verifyNumber = false;
+        return 'não é possível gerar um número de telefone com esses valores'
+      }
+    }
+    return verifyNumber; 
+};
+
+const generatePhoneNumber = (array) => {
+  let validation = verifArray(array); 
+  if (validation === true) {
+    const formated = (`(${array.slice(0, 2).join('')}) ${array.slice(2 , 7).join('')}-${array.slice(7).join('')}`);
+    return formated; 
+  }
+  return validation
+}
 
 // Desafio 12 -  Crie a função triangleCheck
 
